7e7cb5175ef2e1c877bfe153cb5a79d18661369d
Adrian Besciak <adrian.besciak99@gmail.com>
2024-01-08 20:18:58 +0100

    added timer run each 100ms, small code refactor

--- IWISUM_project/f1tenth_controller.py
+++ IWISUM_project/f1tenth_controller.py
@@ -4,40 +4,55 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from ackermann_msgs.msg import AckermannDriveStamped
 
+def fuzzy_logic_model(lidar_msg):
+    angle_min = lidar_msg.angle_min
+    angle_max = lidar_msg.angle_max
+    angle_increment = lidar_msg.angle_increment
+    range_min = lidar_msg.range_min
+    range_max = lidar_msg.range_max
+    ranges = lidar_msg.ranges # list of measurements
+
+    result = {'velocity': 1, 'acceleration': 0.1, 'steering_wheel_angle': 0.5}
+    return result
+
 class F1tenthController(Node):
 
     def __init__(self):
         super().__init__('f1tenth_controller')
+        self.last_lidar_record = None
+        self.publisher_ = self.create_publisher(AckermannDriveStamped, 'drive', 10)
+
         self.subscription = self.create_subscription(
             LaserScan,
             'scan',
             self.lidar_callback,
             10)
-        self.publisher_ = self.create_publisher(AckermannDriveStamped, 'drive', 10)
-        self.prepare_and_send_drive_message(1.0, 0.5, 0.5)
-        # self.subscription  # prevent unused variable warning
+
+        timer_period_s = 0.1  # seconds
+        self.timer = self.create_timer(timer_period_s, self.timer_callback)
 
     def lidar_callback(self, msg):
-        angle_min = msg.angle_min
-        angle_max = msg.angle_max
-        angle_increment = msg.angle_increment
-        range_min = msg.range_min
-        range_max = msg.range_max
-        ranges = msg.ranges # list of measurements
-        # self.get_logger().info(f'Received measuremnt[angle_min: {angle_min}, angle_max: {angle_max}, angle_increment: {angle_increment}, range_max: {range_max}]')
+        self.last_lidar_record = msg
 
     def prepare_and_send_drive_message(self, velocity, angle, acceleration):
         msg = AckermannDriveStamped()
 
         msg.header.stamp = self.get_clock().now().to_msg()
         msg.header.frame_id = 'f1tenth_controller'
-        msg.drive.speed = velocity
-        msg.drive.acceleration = acceleration
+        msg.drive.speed = float(velocity)
+        msg.drive.acceleration = float(acceleration)
         msg.drive.jerk = 0.1
-        msg.drive.steering_angle = angle
+        msg.drive.steering_angle = float(angle)
 
         self.get_logger().info(f"Prepared ackermann message: {msg}")
         self.publisher_.publish(msg)
+
+    def timer_callback(self):
+        if self.last_lidar_record != None:
+            self.get_logger().info('Processing lidar records')
+            result = fuzzy_logic_model(self.last_lidar_record)
+            print(result)
+            self.prepare_and_send_drive_message(result['velocity'], result['steering_wheel_angle'], result['acceleration'])
 
 
 
